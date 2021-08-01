51a643acc9520368b852d17b6bf5560cf1376bb3
srhimel <himusr@gmail.com>
2021-08-01 14:49:06 +0600

    added

---
+++ array.js
@@ -0,0 +1,36 @@
+
+
+
+var friendName = ['naim', 'rabbi', 'munna', 'tamim '];
+friendName.push('mita');
+friendName.push('sagor');
+
+friendName.unshift('sifat', 'rony');
+friendName.shift();
+
+
+
+// friendName[1] = 'mita';
+console.log(friendName);
+
+
+
+
+
+
+
+
+
+//console.log(friendName.indexOf('rabbi'));
+
+
+
+
+
+
+
+
+
+
+
+//console.log(friendName.length);
---
+++ comparison.js
@@ -0,0 +1,13 @@
+var boyos, sallery, buldingAce;
+
+boyos = 22;
+sallery = 50000;
+buldingAce = true;
+
+if (boyos > 21 && sallery > 40000 && buldingAce == true) {
+    console.log("koci mal is ready for you");
+} else if (boyos > 21 && sallery > 40000 || buldingAce == true) {
+    console.log("valo meye but gorib");
+} else {
+    console.log("tor kopale bia nai ");
+}
